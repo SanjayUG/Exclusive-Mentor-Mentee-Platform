ff2ce0fb95ec217e4a8605bff3975b600094c700
SanjayUG <ganjekarsanjay6@gmail.com>
2025-01-02 13:45:44 +0530

    docs: updated .env file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,3 +1,24 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+// import tailwindcss from 'tailwindcss';
+// import autoprefixer from 'autoprefixer';
+
+// // Vite configuration
+// export default defineConfig({
+//   plugins: [react()],
+//   css: {
+//     postcss: {
+//       plugins: [tailwindcss(), autoprefixer()],
+//     },
+//   },
+//   server: {
+//     proxy: {
+//       '/api': 'http://localhost:3001', // Proxy requests starting with '/api' to your backend
+//     },
+//   },
+// });
+
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from 'tailwindcss';
@@ -13,7 +34,7 @@ export default defineConfig({
   },
   server: {
     proxy: {
-      '/api': 'http://localhost:3001', // Proxy requests starting with '/api' to your backend
+      '/api': process.env.VITE_API_BASE_URL || 'http://localhost:3001', // Use VITE_API_BASE_URL from .env or default to localhost
     },
   },
 });
